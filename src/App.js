ac1189db34833130c08fe3718992b243e62f5d18
amanwild <amansahu1130@gmail.com>
2022-09-04 20:43:22 +0530

    news

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c ='Aman'
+  render() {
+    return (
+      <div>Hellow {this.c}</div>
+    )
+  }
+}
